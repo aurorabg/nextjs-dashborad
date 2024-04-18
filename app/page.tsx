3ad6016073e1dd936007e9d79eca87becab7aa89
DESKTOP-NDT9U8D\aurora.balaguer <aurora.balaguer@refineria.es>
2024-04-18 13:14:25 +0200

    Updating next

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
+import Image from 'next/image'
 
 export default function Page() {
   return (
@@ -17,6 +19,9 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
+          <div
+              className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+            />
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
@@ -26,6 +31,10 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <img
+            src="/hero-desktop.png"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
         </div>
       </div>
     </main>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+// module.exports = {
+//     images: {
+//         remotePatterns: [
+//             {
+//                 protocol: 'https',
+//                 hostname: 's3.amazonaws.com',
+//                 port: '',
+//                 pathname: '/my-bucket/**',
+//             },
+//         ],
+//     },
+// }
+
 module.exports = nextConfig;
